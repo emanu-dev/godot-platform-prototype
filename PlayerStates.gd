ebcfcc4b2e6f798889eda8873d41f0eaa20f1563
emanu-dev <emanuel.prado.dev@gmail.com>
2019-07-12 21:15:42 -0300

    Add Attack to state machine

--- PlayerStates.gd
+++ PlayerStates.gd
@@ -1,7 +1,10 @@
 extends StateMachine
 
+var animFinished = false
+
 func _ready():
 	add_state("idle")
+	add_state("idleOffense")
 	add_state("duck")
 	add_state("walk")
 	add_state("jump")
@@ -12,15 +15,16 @@ func _ready():
 	call_deferred("set_state", states.idle)	
 
 func _state_logic(delta):
+	print (animFinished)
 	parent._handle_move_input()
 	parent._apply_gravity()
-	if [states.idle, states.jump, states.walk, states.fall].has(state):
+	if [states.idle, states.idleOffense, states.jump, states.walk, states.fall].has(state):
 		parent._apply_movement()
 	
 	if state == states.duck:
 		parent.currentAnimation = "Duck"
 	
-	print (state)
+	#print (state)
 
 	
 func _get_transition(delta):
@@ -30,26 +34,56 @@ func _get_transition(delta):
 				if parent.motion.y < 0:
 					return states.jump
 				if parent.motion.y >= 0:
-						return states.fall					
-			if Input.is_action_pressed("ui_down"):
-				return states.duck
+					return states.fall					
 			else:
-				if parent.motion.x != 0:
+				if Input.is_action_pressed("ui_accept"):
+					return states.attack
+				elif Input.is_action_pressed("ui_down"):
+					return states.duck					
+				elif parent.motion.x != 0:
 					return states.walk
-				if parent.motion.x == 0:
+				elif parent.motion.x == 0:
 					return states.idle
+					
+		states.idleOffense:
+			if !parent.is_on_floor():
+				if parent.motion.y < 0:
+					return states.jump
+				if parent.motion.y >= 0:
+					return states.fall					
+			else:
+				if Input.is_action_pressed("ui_accept"):
+					return states.attack
+				elif Input.is_action_pressed("ui_down"):
+					return states.duck					
+				elif parent.motion.x != 0:
+					return states.walk
+				elif parent.motion.x == 0:
+					return states.idleOffense
+											
 		states.jump:
+			if parent.is_on_floor():
+				return states.idle			
 			if parent.motion.y >= 0:
-					return states.fall
+				return states.fall
 		states.fall:
-			if parent.motion.y == 0:
-					return states.idle
+			if parent.is_on_floor():
+				return states.idle
+			if parent.motion.y < 0:
+				return states.jump				
 		states.duck:
 			if Input.is_action_just_released("ui_down"):
 				return states.idle
+		states.attack:
+			if animFinished == true:
+				animFinished = false
+				return states.idleOffense
+				
+	return null
+	
 	
 func _enter_state(new_state, old_state):
-	match state:
+	match new_state:
 		states.idle:
 			parent.currentAnimation = "Idle"
 		states.walk:
@@ -60,7 +94,14 @@ func _enter_state(new_state, old_state):
 			parent.currentAnimation = "Fall"
 		states.duck:
 			parent.currentAnimation = "Duck"
+		states.attack:
+			parent.currentAnimation = "Attack"
+		states.idleOffense:
+			parent.currentAnimation = "IdleOffense"
 	pass
 	
 func _exit_state(old_state, new_state):
 	pass
+
+func _on_anim_finished(anim_name):
+	animFinished = true
