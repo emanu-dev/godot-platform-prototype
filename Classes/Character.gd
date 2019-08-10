09d0607cab43deddb6017542d04ea9c9b1f9e518
emanu-dev <emanuel.prado.dev@gmail.com>
2019-08-10 19:33:53 -0300

    Update Character class

--- Classes/Character.gd
+++ Classes/Character.gd
@@ -1,3 +1,36 @@
 extends KinematicBody2D
-
 class_name Character
+
+var animator = null setget set_animator
+
+const UP = Vector2(0, -1)
+const GRAVITY = 16
+
+var motion = Vector2()
+var gravityModifier = 1 setget set_gravity_modifier, get_gravity_modifier
+var health = 100
+
+
+func set_animator(node):
+	animator = node
+
+func set_gravity_modifier(newvalue):
+	gravityModifier = newvalue
+	
+func get_gravity_modifier():
+	return gravityModifier
+
+func apply_motion():
+	motion = move_and_slide(motion, UP)
+
+func apply_gravity():
+	motion.y += GRAVITY * gravityModifier
+
+func play_anim(animName):
+	animator.play(animName)
+	
+func attack():
+	pass
+	
+func die():
+	pass
--- PlayerBehavior.gd
+++ PlayerBehavior.gd
@@ -1,12 +1,9 @@
 extends Character
 
 #OnreadyConst
-onready var animPlayer = get_node("Sprite/playerAnimator")
 onready var attackArea = get_node("AttackArea/AttackCollision")
 
 #Const
-const UP = Vector2(0, -1)
-const GRAVITY = 16
 const SPEED = 192
 const JUMP_HEIGHT = -420
 const FALL_MULTIPLIER = 1.6
@@ -15,46 +12,40 @@ const AIR_MOVEMENT = .8
 const KNOCKBACK_FORCE = 256
 
 #Movement
-var motion = Vector2()
-var gravityModifier = 1
 var attackAreaPos = null
 
 #Game Variables
-var health = 100
 var touchEnemy = false
 
 func _ready():
+	set_animator(get_node("Sprite/playerAnimator"))
 	attackAreaPos = attackArea.position	
 
 func _process(delta):
 	pass
 
 ################ PUBLIC FUNCTIONS #######################
-func apply_gravity():
-	motion.y += GRAVITY * gravityModifier	
+
 
 func handle_jump():
 	if is_on_floor():
-		gravityModifier = 1
+		set_gravity_modifier(1)
 		
 		if Input.is_action_just_pressed("ui_select"):
 			motion.y = JUMP_HEIGHT		
 	else:
 		# Multiply fall speed for a better jump
 		if motion.y > 0:		
-			gravityModifier = FALL_MULTIPLIER
+			set_gravity_modifier(FALL_MULTIPLIER)
 		# Multiply jump speed for a pressure sensitive jump
 		elif motion.y < 0 && !Input.is_action_pressed("ui_select"):		
-			gravityModifier = JUMP_MULTIPLIER
+			set_gravity_modifier(JUMP_MULTIPLIER)
 			
 func handle_move():
 	if is_on_floor():
 		motion.x = SPEED * _input_horizontal()
 	else:
 		motion.x = (SPEED * AIR_MOVEMENT) * _input_horizontal()
-
-func apply_motion():
-	motion = move_and_slide(motion, UP)	
 
 func flip_sprite():
 	#Flip sprite according to direction
@@ -67,29 +58,20 @@ func flip_sprite():
 
 func damage_by_enemy(body):
 	if body.has_method("_inflict_damage"):
-		_reset_gravity_modifier()
+		set_gravity_modifier(1)
 		_set_damage(body._inflict_damage())
 		_dmg_knock_back(body.position)
 		_set_damage_box_disabled(true)
 		apply_motion()
 
-func play_anim(anim):
-	animPlayer.play(anim)
-
 ################ PRIVATE FUNCTIONS #######################
-func _reset_gravity_modifier():
-	gravityModifier = 1
-
 func _set_damage(dmg):
 	if !touchEnemy:
 		if health - dmg >= 0:
 			health -= dmg
 		else:
 			health -= 0
-		
 		touchEnemy = true
-	else:
-		return null
 	
 func _dmg_knock_back(enemyPosition):
 	#move(Vector2(0,-1).rotated(get_global_pos().angle_to_point(enemyPosition))*speed*delta)
